40ae946613582af14aa42f035162180fd50e3a74
Omar Einea <eineao@gmail.com>
2017-08-11 17:53:30 +0300

    Move .bashrc & deploy.sh From BackEnd To DigitalOcean/root

---
+++ DigitalOcean/root/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Go to project dir
+cd /home
+
+# Clean up cached static files
+rm -r static Website/static/min
+
+# Backup database file
+mv db.sqlite3 db.save
+
+# Sync back-end repo with GitHub
+git fetch --all
+git reset --hard origin/master
+
+# Sync front-end repo with GitHub
+cd Website/static
+git fetch --all
+git reset --hard origin/master
+cd ../..
+
+# Restore database file
+mv db.save db.sqlite3
+
+# Compress and collect static files
+python3 manage.py compress
+python3 manage.py collectstatic
+
+# Clean up uncompressed static files
+rm -r static/css static/js
+
+# Restart Gunicorn and Nginx
+systemctl restart gunicorn
+systemctl restart nginx
