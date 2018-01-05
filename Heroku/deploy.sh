f4e95eee0020028123e2ce414f8b7000d93caa70
Omar Einea <eineao@gmail.com>
2018-01-05 17:19:07 +0300

    Add Commit & Push Changes To Heroku, But Ask Beofre It

--- Heroku/deploy.sh
+++ Heroku/deploy.sh
@@ -16,3 +16,14 @@ python ../manage.py compress --force
 # Cleanup front-end Git files and useless files
 cd static
 rm -rf .git .gitignore css js LICENSE.md README.md requirements.txt
+
+# Ask whether to commit and push to Heroku or not
+read -r -p "Deploy to Heroku? [y/N] " response
+if [[ ${response,,} =~ ^(yes|y)$ ]]
+then
+
+# Commit new changes and push to Heroku
+git add --all
+git commit -m "Added FrontEnd And BackEnd Cumulative Updates"
+git push heroku master
+fi
