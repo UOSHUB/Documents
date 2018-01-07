ae0367d08151dc9b42e436feae505ea92e9e9eb0
Omar Einea <eineao@gmail.com>
2018-01-07 17:31:00 +0300

    Show Deployment Comments As Progress On Console

--- Heroku/deploy.sh
+++ Heroku/deploy.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-# Fetch new back-end changes and wait for cherry-picking
+echo "Fetching new back-end changes and wait for cherry-picking"
 git fetch origin
 "C:\Program Files\Git\git-bash.exe"
 
-# Remove & clone front-end repo from GitHub
+echo "Removing then cloning front-end repo from GitHub"
 cd Website
 rm -rf static
 git clone https://github.com/UOSHUB/FrontEnd static
 
-# Download static requirements compress static files
+echo "Downloading static requirements & compress static files"
 python download.py
 python ../manage.py compress --force
 
-# Cleanup front-end Git files and useless files
+echo "Cleaning up front-end Git files and useless files"
 cd static
 rm -rf .git .gitignore css js LICENSE.md README.md requirements.txt
 
-# Ask whether to commit and push to Heroku or not
+echo "Checking whether to commit and push to Heroku or not"
 read -r -p "Deploy to Heroku? [y/N] " response
 if [[ ${response,,} =~ ^(yes|y)$ ]]
 then
 
-# Commit new changes and push to Heroku
+echo "Committing new changes and pushing to Heroku"
 git add --all
 git commit -m "Added FrontEnd And BackEnd Cumulative Updates"
 git push heroku master
